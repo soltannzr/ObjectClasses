6dce592b3281616c6a303bad0a3d4fc846ca0538
Soltan Nazarov <sltn.soft@gmail.com>
2023-08-25 00:25:09 +0300

    Sorting elements of array

--- src/Arrays/SortElementsOfArray.java
+++ src/Arrays/SortElementsOfArray.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class SortElementsOfArray {
     public static void main(String[] args) {
+
         Scanner inp = new Scanner(System.in);
         System.out.print("Enter the size of an array n : ");
+
         int n=inp.nextInt();
         int[] arr = new int[n];
         int counter = 1;
+
         System.out.println("Enter the elements of the array :");
         for (int i = 0; i<arr.length; i++){
             System.out.print("Enter " + counter + ". element : " );
