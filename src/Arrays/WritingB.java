cfc9dc78550cfa50f36df4fb99e36d7d07469f1e
Soltan Nazarov <sltn.soft@gmail.com>
2023-08-24 03:51:46 +0300

    Letter B

---
+++ src/Arrays/WritingB.java
@@ -0,0 +1,28 @@
+package Arrays;
+public class WritingB {
+    public static void main(String[] args) {
+        String [][] arr = new String[7][4];
+
+        for(int i = 0; i<arr.length; i++){
+            for(int j = 0; j<arr[i].length; j++){
+                if(j==0 || i==0 || (i==3 && j!=3) || (j==3 && i!=3)){
+                    arr[i][j] = "* ";
+                }
+                else if (i==6){
+                    arr[i][j]="* ";
+                }
+                else {
+                     arr[i][j] = "  ";
+                 }
+            }
+        }
+
+        for (String[] row : arr){
+            for (String col: row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+
+    }
+}
